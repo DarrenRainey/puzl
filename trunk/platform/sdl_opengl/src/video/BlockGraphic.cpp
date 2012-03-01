f7bc091857edb76df96b8c8837a52f5f984e04be
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2012-03-01 04:41:51 +0000

    Added makefile to build static library for platform sdl_opengl (linux, mac, windows). Required minor changes to include/source files.

--- trunk/platform/sdl_opengl/src/video/BlockGraphic.cpp
+++ trunk/platform/sdl_opengl/src/video/BlockGraphic.cpp
@@ -30,7 +30,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // INCLUDES ======================================================================
 
-#include "BlockGraphic.h"		// BlockGraphic header file
+#include <puzl/video/BlockGraphic.h>		// BlockGraphic header file
 
 // DEFINES =======================================================================
 
