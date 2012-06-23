6c636766025e111552c33014a55939d1cbf04d79
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2012-06-23 11:53:03 +0000

    Added starts of JS sprite class. Added starts of button class.

---
+++ trunk/language/js/platform/html5/puzl/utility/Button.js
@@ -0,0 +1,37 @@
+// button functions
+function Button( xPosition, yPosition, width, height )
+{
+  this.xPosition = xPosition;
+  this.yPosition = yPosition;
+  this.width     = width;
+  this.height    = height;
+
+  this.xEnd = xPosition + width  - 1;
+  this.yEnd = yPosition + height - 1;
+}
+
+Button.prototype.checkClicked = function()
+{
+  //console.log( mouseX + ":" + mouseY );
+  if( mouseY < this.yPosition )
+  {
+    return false;
+  }
+
+  if( mouseY > this.yEnd )
+  {
+    return false;
+  }
+
+  if( mouseX < this.xPosition )
+  {
+    return false;
+  }
+
+  if( mouseX > this.xEnd )
+  {
+    return false;
+  }
+
+  return true;
+};
