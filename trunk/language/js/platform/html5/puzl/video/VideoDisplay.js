25e95698053d6d293a4ed9aee29af74f4d4120c0
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2012-06-30 13:38:01 +0000

    Added basis of determining coordinates between different canvas based VideoImages linked by a VideoDisplay.

--- trunk/language/js/platform/html5/puzl/video/VideoDisplay.js
+++ trunk/language/js/platform/html5/puzl/video/VideoDisplay.js
@@ -11,128 +11,202 @@ function VideoDisplay()
   this.centerDisplay;
   this.xOffset;
   this.yOffset;
+
+  this.videoImageList;
   
   this.fullScreen;
   
   this.foregroundColor;
   
-  this.constructor = function()
-  {
-    //GlobalVideoDisplay = this;
-    
-    this.setFullScreen( false );
-
-    this.centerDisplay = true;
-    this.maintainAspectRatio = true;
-    this.setRealDimensions( this.getHeight(), this.getWidth() );
-    
-    this.setBackgroundColor( "rgb(0,0,0)" );
-  };
-  
-  this.setFullScreen = function( fullScreen )
-  {
-    this.fullScreen = fullScreen;
-  };
-
-  this.getHeight = function()
-  {
-    //return document.height;
-    /*console.log( document.body.scrollHeight +":"+ document.documentElement.scrollHeight+":"+
-                     document.body.offsetHeight+":"+document.documentElement.offsetHeight+":"+
-                     document.body.clientHeight+":"+document.documentElement.clientHeight );*/
-    
-    return Math.max( /*Math.max( document.body.scrollHeight, document.documentElement.scrollHeight ),*/
-                     Math.max( document.body.offsetHeight, document.documentElement.offsetHeight ),
-                     Math.max( document.body.clientHeight, document.documentElement.clientHeight ) );
-  };
-  
-  this.getWidth = function()
-  {
-    //return document.width;
-    /*console.log( document.body.scrollWidth +":"+ document.documentElement.scrollWidth+":"+
-                     document.body.offsetWidth+":"+document.documentElement.offsetWidth+":"+
-                     document.body.clientWidth+":"+document.documentElement.clientWidth );*/
-                     
-    return Math.max( /*Math.max( document.body.scrollWidth, document.documentElement.scrollWidth ),*/
-                     Math.max( document.body.offsetWidth, document.documentElement.offsetWidth ),
-                     Math.max( document.body.clientWidth, document.documentElement.clientWidth ) );
-  };
-
-  this.setRealDimensions = function( width, height )
-  {
-    this.realWidth  = width;
-    this.realHeight = height;
-
-    // Determine scale.
-    this.xScale = Math.floor( this.getWidth()  / this.realWidth );
-    if( this.xScale <= 0 )
-    {
-      this.xScale = 1;
-    }
-    
-    this.yScale = Math.floor( this.getHeight() / this.realHeight );
-    if( this.yScale <= 0 )
-    {
-      this.yScale = 1;
-    }
-
-    if( this.maintainAspectRatio )
-    {
-      this.xScale = this.yScale = Math.min( this.xScale, this.yScale );
-    }
-
-    // Determine position offset.
-    if( this.centerDisplay )
-    {
-      this.xOffset = Math.floor( ( this.getWidth()  - ( this.realWidth  * this.xScale ) ) / 2 );
-      this.yOffset = Math.floor( ( this.getHeight() - ( this.realHeight * this.yScale ) ) / 2 );
-    }
-    else
-    {
-      this.xOffset = 0;
-      this.yOffset = 0;
-    }
-  };
-  
-  this.clear = function()
-  {
-    // TODO: Determine what this function should do.
-    // Perhaps clear list of registered VideoObjects and null their links to
-    // this display.
-  };
-  
-  this.setBackgroundColor = function( color )
-  {
-    //this.backgroundColor = "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
-    document.body.style.backgroundColor = color;
-  };
-  
-  this.setForegroundColor = function( color )
-  {
-    //this.foregroundColor = "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
-    this.foregroundColor = color;
-  };
-  
-  this.drawRectangle = function( xPosition, yPosition, width, height )
-  {
-    //this.context.fillStyle = this.foregroundColor;
-    //this.context.fillRect( xPosition, yPosition, width, height );
-  };
-
-  this.drawRectangle = function( canvas, xPosition, yPosition, width, height )
-  {
-    var canvasContext = GetCanvasContext2D( canvas );
-    canvasContext.fillStyle = this.foregroundColor;
-    canvasContext.fillRect( xPosition, yPosition, width, height );
-  };
-  
-  /*this.getColorRgb = function( red, green, blue )
-  {
-    var color = new Color();
-    color.setRgb( red, green, blue );
-    return color;
-  };*/
-  
   this.constructor();
   return this;
 }
+
+VideoDisplay.prototype.constructor = function()
+{
+  //GlobalVideoDisplay = this;
+
+  this.setFullScreen( false );
+
+  this.centerDisplay = true;
+  this.maintainAspectRatio = true;
+  this.setRealDimensions( this.getHeight(), this.getWidth() );
+
+  this.setBackgroundColor( "rgb(0,0,0)" );
+
+  this.videoImageList = new Array();
+};
+
+VideoDisplay.prototype.setFullScreen = function( fullScreen )
+{
+  this.fullScreen = fullScreen;
+};
+
+VideoDisplay.prototype.getHeight = function()
+{
+  //return document.height;
+  /*console.log( document.body.scrollHeight +":"+ document.documentElement.scrollHeight+":"+
+                    document.body.offsetHeight+":"+document.documentElement.offsetHeight+":"+
+                    document.body.clientHeight+":"+document.documentElement.clientHeight );*/
+
+  return Math.max( /*Math.max( document.body.scrollHeight, document.documentElement.scrollHeight ),*/
+                    Math.max( document.body.offsetHeight, document.documentElement.offsetHeight ),
+                    Math.max( document.body.clientHeight, document.documentElement.clientHeight ) );
+};
+
+VideoDisplay.prototype.getWidth = function()
+{
+  //return document.width;
+  /*console.log( document.body.scrollWidth +":"+ document.documentElement.scrollWidth+":"+
+                    document.body.offsetWidth+":"+document.documentElement.offsetWidth+":"+
+                    document.body.clientWidth+":"+document.documentElement.clientWidth );*/
+
+  return Math.max( /*Math.max( document.body.scrollWidth, document.documentElement.scrollWidth ),*/
+                    Math.max( document.body.offsetWidth, document.documentElement.offsetWidth ),
+                    Math.max( document.body.clientWidth, document.documentElement.clientWidth ) );
+};
+
+VideoDisplay.prototype.setRealDimensions = function( width, height )
+{
+  this.realWidth  = width;
+  this.realHeight = height;
+
+  // Determine scale.
+  this.xScale = Math.floor( this.getWidth()  / this.realWidth );
+  if( this.xScale <= 0 )
+  {
+    this.xScale = 1;
+  }
+
+  this.yScale = Math.floor( this.getHeight() / this.realHeight );
+  if( this.yScale <= 0 )
+  {
+    this.yScale = 1;
+  }
+
+  if( this.maintainAspectRatio )
+  {
+    this.xScale = this.yScale = Math.min( this.xScale, this.yScale );
+  }
+
+  // Determine position offset.
+  if( this.centerDisplay )
+  {
+    this.xOffset = Math.floor( ( this.getWidth()  - ( this.realWidth  * this.xScale ) ) / 2 );
+    this.yOffset = Math.floor( ( this.getHeight() - ( this.realHeight * this.yScale ) ) / 2 );
+  }
+  else
+  {
+    this.xOffset = 0;
+    this.yOffset = 0;
+  }
+};
+
+VideoDisplay.prototype.clear = function()
+{
+  // Clear list of registered VideoObjects and null their links to
+  // this display.
+  var length = this.videoImageList.length;
+  var index;
+  for( index = 0; index < length; index++ )
+  {
+    this.videoImageList[index].setDisplay( null );
+  }
+
+  this.videoImageList = [];
+};
+
+VideoDisplay.prototype.setBackgroundColor = function( color )
+{
+  //this.backgroundColor = "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
+  document.body.style.backgroundColor = color;
+};
+
+VideoDisplay.prototype.setForegroundColor = function( color )
+{
+  //this.foregroundColor = "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
+  this.foregroundColor = color;
+};
+
+VideoDisplay.prototype.drawRectangle = function( xPosition, yPosition, width, height )
+{
+  //this.context.fillStyle = this.foregroundColor;
+  //this.context.fillRect( xPosition, yPosition, width, height );
+};
+
+VideoDisplay.prototype.drawRectangle = function( canvas, xPosition, yPosition, width, height )
+{
+  var canvasContext = GetCanvasContext2D( canvas );
+  canvasContext.fillStyle = this.foregroundColor;
+  canvasContext.fillRect( xPosition, yPosition, width, height );
+};
+
+/*VideoDisplay.prototype.getColorRgb = function( red, green, blue )
+{
+  var color = new Color();
+  color.setRgb( red, green, blue );
+  return color;
+};*/
+
+VideoDisplay.prototype.addVideoImage = function( videoImage )
+{
+  // TODO: Make sure videoImage is not already in the list.
+  videoImage.setDisplay( this );
+  this.videoImageList[this.videoImageList.length] = videoImage;
+};
+
+VideoDisplay.prototype.addVideoImage = function( videoImage )
+{
+  // TODO: Make sure videoImage is not already in the list.
+  videoImage.setDisplay( this );
+  this.videoImageList[this.videoImageList.length] = videoImage;
+};
+
+VideoDisplay.prototype.removeVideoImage = function( videoImage )
+{
+  var length = this.videoImageList.length;
+  var index;
+  for( index = 0; index < length; index++ )
+  {
+    if( videoImage == this.videoImageList[index] )
+    {
+      videoImage.setDisplay( null );
+      this.videoImageList.splice( index, 1 );
+      return;
+    }
+  }
+};
+
+VideoDisplay.prototype.determineTopLeft = function()
+{
+  var videoImage;
+  var length = this.videoImageList.length;
+  if( length < 1 )
+  {
+    this.top  = 0;
+    this.left = 0;
+    return;
+  }
+  
+  this.top  = this.videoImageList[index].getYPosition();
+  this.left = this.videoImageList[index].getXPosition();
+
+  var currentTop;
+  var currentLeft;
+  for( var index = 0; index < length; index++ )
+  {
+    videoImage  = this.videoImageList[index];
+    
+    currentTop  = videoImage.getYPosition();
+    if( currentTop < this.top )
+    {
+      this.top = currentTop;
+    }
+
+    currentLeft = videoImage.getXPosition();
+    if( currentLeft < this.left )
+    {
+      this.left = currentLeft;
+    }
+  }
+};
--- trunk/language/js/platform/html5/puzl/video/VideoImage.js
+++ trunk/language/js/platform/html5/puzl/video/VideoImage.js
@@ -10,147 +10,164 @@ function VideoImage()
 
   this.width;
   this.height;
-    
-  this.constructor = function()
-  {
-    if( arguments.length > 0 )
-    {
-      if( arguments.length > 2 )
-      {
-        //var argument0 = arguments[0];
-        //if( typeof arguments[0] )
-      }
-    }
-    else
-    {
 
-    }
-
-    this.canvasID = ++CanvasIdCounter + "canvas";
-    this.canvas   = CreateOffScreenCanvas( this.canvasID );
-    this.display  = null;
-
-    this.filename = "";
-
-    this.setRealDimensions( 1, 1 );
-    //this.width  = 1;
-    //this.height = 1;
-  };
-
-  this.setRealDimensions = function( width, height )
-  {
-    SetCanvasDimensions( this.canvas, width, height );
-    
-    if( this.display == null )
-    {
-      this.width  = width;
-      this.height = height;
-    }
-  };
-
-  this.getRealWidth = function( width )
-  {
-    return this.canvas.width;
-  };
-
-  this.getRealHeight = function( height )
-  {
-    return this.canvas.height;
-  };
-  
-  this.setDimensions = function( width, height )
-  {
-    if( this.display == null )
-    {
-      this.setRealDimensions( width, height );
-    }
-    else
-    {
-      this.width  = width;
-      this.height = height;
-
-      this.setRealDimensions( width  * this.display.xScale,
-                              height * this.display.yScale );
-    }
-  };
-
-  this.getWidth = function( width )
-  {
-    return this.width;
-  };
-
-  this.getHeight = function( height )
-  {
-    return this.height;
-  };
-
-  this.setPosition = function( xPosition, yPosition )
-  {
-    // NOTE: Should this only happen with a display.
-    if( this.display != null )
-    {
-      //console.log( this.display.xOffset );
-      SetCanvasPosition( this.canvas,
-                         ( xPosition * this.display.xScale ) + this.display.xOffset,
-                         ( yPosition * this.display.yScale ) + this.display.yOffset );
-    }
-  };
-
-  this.fill = function( colorRgb )
-  {
-    var context = GetCanvasContext2D( this.canvas );
-    context.fillStyle = colorRgb;
-    context.fillRect( 0, 0, this.canvas.width, this.canvas.height );
-  };
-
-  this.setDisplay = function( display )
-  {
-    //console.log( "VideoImage::setDisplay()" );
-    if( display == this.display )
-    {
-      return;
-    }
-    
-    if( display == null && this.display != null )
-    {
-      // Turn this page anchored canvas to an offscreen canvas.
-      this.canvas = OnScreenToOffScreenCanvas( this.canvas );
-      this.display = null;
-    }
-    else
-    {
-      // Turn this offscreen canvas to a page anchored canvas.
-      if( this.display != null )
-      {
-        // NOTE: Multiple displays are not really supported right now;
-        // so, just act like nothing happened.
-        //this.setDisplay( null );
-        return;
-      }
-
-      this.canvas = OffScreenToOnScreenCanvas( this.canvas );
-      this.display = display;
-    }
-  };
-
-  this.load = function( filename )
-  {
-    //console.log( "VideoImage::load()" );
-    this.filename = filename;
-    GlobalVideoSystem.queueVideoImage( this, this.filename );
-  };
-
-  // Temporary?
-  this.getCanvas = function()
-  {
-    return this.canvas;
-  };
-
-  this.getContext = function()
-  {
-    return GetCanvasContext2D( this.canvas );
-  };
+  this.xPosition;
+  this.yPosition;
 
   this.constructor();
   return this;
 }
+
+VideoImage.prototype.constructor = function()
+{
+  if( arguments.length > 0 )
+  {
+    if( arguments.length > 2 )
+    {
+      //var argument0 = arguments[0];
+      //if( typeof arguments[0] )
+    }
+  }
+  else
+  {
+
+  }
+
+  this.canvasID = ++CanvasIdCounter + "canvas";
+  this.canvas   = CreateOffScreenCanvas( this.canvasID );
+  this.display  = null;
+
+  this.filename = "";
+
+  this.setRealDimensions( 1, 1 );
+  //this.width  = 1;
+  //this.height = 1;
+
+  this.xPosition = 0;
+  this.yPosition = 0;
+};
+
+VideoImage.prototype.setRealDimensions = function( width, height )
+{
+  SetCanvasDimensions( this.canvas, width, height );
+
+  if( this.display == null )
+  {
+    this.width  = width;
+    this.height = height;
+  }
+};
+
+VideoImage.prototype.getRealWidth = function()
+{
+  return this.canvas.width;
+};
+
+VideoImage.prototype.getRealHeight = function()
+{
+  return this.canvas.height;
+};
+
+VideoImage.prototype.setDimensions = function( width, height )
+{
+  if( this.display == null )
+  {
+    this.setRealDimensions( width, height );
+  }
+  else
+  {
+    this.width  = width;
+    this.height = height;
+
+    this.setRealDimensions( width  * this.display.xScale,
+                            height * this.display.yScale );
+  }
+};
+
+VideoImage.prototype.getWidth = function()
+{
+  return this.width;
+};
+
+VideoImage.prototype.getHeight = function()
+{
+  return this.height;
+};
+
+VideoImage.prototype.getXPosition = function()
+{
+  return this.xPosition;
+};
+
+VideoImage.prototype.getYPosition = function()
+{
+  return this.yPosition;
+};
+
+VideoImage.prototype.setPosition = function( xPosition, yPosition )
+{
+  // NOTE: Should this only happen with a display.
+  if( this.display != null )
+  {
+    //console.log( this.display.xOffset );
+    this.xPosition = ( xPosition * this.display.xScale ) + this.display.xOffset;
+    this.yPosition = ( yPosition * this.display.yScale ) + this.display.yOffset;
+    
+    SetCanvasPosition( this.canvas,
+                       this.xPosition, this.yPosition );
+  }
+};
+
+VideoImage.prototype.fill = function( colorRgb )
+{
+  var context = GetCanvasContext2D( this.canvas );
+  context.fillStyle = colorRgb;
+  context.fillRect( 0, 0, this.canvas.width, this.canvas.height );
+};
+
+VideoImage.prototype.setDisplay = function( display )
+{
+  //console.log( "VideoImage::setDisplay()" );
+  if( display == this.display )
+  {
+    return;
+  }
+
+  if( display == null && this.display != null )
+  {
+    // Turn this page anchored canvas to an offscreen canvas.
+    this.display = null;
+  }
+  else
+  {
+    // Turn this offscreen canvas to a page anchored canvas.
+    if( this.display != null )
+    {
+      // NOTE: Multiple displays are not really supported right now;
+      // so, just act like nothing happened.
+      //this.setDisplay( null );
+      return;
+    }
+
+    this.canvas = OffScreenToOnScreenCanvas( this.canvas );
+    this.display = display;
+  }
+};
+
+VideoImage.prototype.load = function( filename )
+{
+  //console.log( "VideoImage::load()" );
+  this.filename = filename;
+  GlobalVideoSystem.queueVideoImage( this, this.filename );
+};
+
+// Temporary?
+VideoImage.prototype.getCanvas = function()
+{
+  return this.canvas;
+};
+
+VideoImage.prototype.getContext = function()
+{
+  return GetCanvasContext2D( this.canvas );
+};
