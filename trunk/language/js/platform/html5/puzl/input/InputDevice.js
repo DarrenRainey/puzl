55fe148e899013bebc6db913218c52fc96209634
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2012-06-30 11:41:31 +0000

    Added basic mouse and joystick support for JS engine.

--- trunk/language/js/platform/html5/puzl/input/InputDevice.js
+++ trunk/language/js/platform/html5/puzl/input/InputDevice.js
@@ -17,71 +17,81 @@ function InputDevice()
   this.input;
   this.stateChange;
   
-  this.stateChangeBufferSize = 0;
-  this.numberOfStateChanges  = 0;
+  this.stateChangeBufferSize;
+  this.numberOfStateChanges;
   
-  this.lastInputId   = -1;
-  this.lastInputType = 0;
-  
-  this.initialize = function()
-  {
-    return 0;
-  };
-  
-  this.shutdown = function()
-  {
-    return 0;
-  };
-  
-  this.check = function( inputId )
-  {
-    return ( this.input[inputId].state & INPUT_STATE_DOWN ) ? true : false;
-  };
-  
-  this.getState = function( inputId )
-  {
-    return this.input[inputId].state;
-  };
-  
-  this.setState = function( inputId, state )
-  {
-    this.input[inputId].state = state;
-    if( state == INPUT_STATE_PRESSED )
-    {
-      this.lastInputId = inputId;
-    }
-    
-    this.stateChange[this.numberOfStateChanges++] = this.input[inputId];
-  };
-  
-  this.getLastInputId = function()
-  {
-    return this.lastInputId;
-  };
-  
-  this.age = function()
-  {
-    if( this.numberOfStateChanges > 0 )
-    {
-      var index;
-      for( index = 0; index < this.numberOfStateChanges; index++ )
-      {
-        var tempStateChange = this.stateChange[index];
-        if( tempStateChange.state == INPUT_STATE_RELEASED )
-        {
-          tempStateChange.state = INPUT_STATE_UP;
-        }
-        else
-        if( tempStateChange.state == INPUT_STATE_PRESSED )
-        {
-          tempStateChange.state = INPUT_STATE_DOWN;
-        }
-      }
-      
-      this.numberOfStateChanges = 0;
-      this.lastInputId          = -1;
-    }
-  };
-  
+  this.lastInputId;
+  this.lastInputType;
+
+  this.constructor();
   return this;
 }
+
+InputDevice.prototype.constructor = function()
+{
+  this.stateChangeBufferSize = 0;
+  this.numberOfStateChanges  = 0;
+
+  this.lastInputId   = -1;
+  this.lastInputType = 0;
+};
+
+InputDevice.prototype.initialize = function()
+{
+  return 0;
+};
+
+InputDevice.prototype.shutdown = function()
+{
+  return 0;
+};
+
+InputDevice.prototype.check = function( inputId )
+{
+  return ( this.input[inputId].state & INPUT_STATE_DOWN ) == 0 ? false : true;
+};
+
+InputDevice.prototype.getState = function( inputId )
+{
+  return this.input[inputId].state;
+};
+
+InputDevice.prototype.setState = function( inputId, state )
+{
+  this.input[inputId].state = state;
+  if( state == INPUT_STATE_PRESSED )
+  {
+    this.lastInputId = inputId;
+  }
+
+  this.stateChange[this.numberOfStateChanges++] = this.input[inputId];
+};
+
+InputDevice.prototypegetLastInputId = function()
+{
+  return this.lastInputId;
+};
+
+InputDevice.prototype.age = function()
+{
+  if( this.numberOfStateChanges > 0 )
+  {
+    var index;
+    for( index = 0; index < this.numberOfStateChanges; index++ )
+    {
+      var tempStateChange = this.stateChange[index];
+      if( tempStateChange.state == INPUT_STATE_RELEASED )
+      {
+        tempStateChange.state = INPUT_STATE_UP;
+      }
+      else
+      if( tempStateChange.state == INPUT_STATE_PRESSED )
+      {
+        tempStateChange.state = INPUT_STATE_DOWN;
+      }
+    }
+
+    this.numberOfStateChanges = 0;
+    this.lastInputId          = -1;
+  }
+};
