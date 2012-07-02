4b65fa9bfd8727d6e8955ee135642e256f25da59
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2012-07-02 02:39:25 +0000

    Expanded GameScreen and refactored GameShell.

--- trunk/language/js/platform/html5/puzl/utility/GameScreen.js
+++ trunk/language/js/platform/html5/puzl/utility/GameScreen.js
@@ -33,37 +33,51 @@ GameScreen.prototype.setSubScreenIndex = function( subScreenIndex )
   this.subScreenIndex = subScreenIndex;
   this.subScreen      = this.subScreenList[this.subScreenIndex];
   //this.initialize     = this.subScreen.initialize; // NOTE: Subscreens shouldn't have their own initialize?
-  this.reset          = this.subScreen.reset;
-  this.input          = this.subScreen.input;
-  this.logic          = this.subScreen.logic;
-  this.draw           = this.subScreen.draw;
+  //this.reset          = this.subScreen.reset;
+  //this.input          = this.subScreen.input;
+  //this.logic          = this.subScreen.logic;
+  //this.draw           = this.subScreen.draw;
 
   // NOTE: Kind of crappily needed for instance runs to recongize
   // this.parentScreen when running subscreen routines.
-  this.parentScreen   = this;
+  //this.parentScreen   = this;
 };
 
 GameScreen.prototype.initialize = function()
 {
+  
+};
 
+GameScreen.prototype.postInitialize = function()
+{
+  
 };
 
 GameScreen.prototype.reset = function()
 {
-
+  this.subScreen.reset();
 };
 
 GameScreen.prototype.input = function()
 {
-
+  this.subScreen.input();
 };
 
 GameScreen.prototype.logic = function()
 {
-  
+  this.subScreen.logic();
 };
 
 GameScreen.prototype.draw = function()
 {
-  
+  this.subScreen.draw();
 };
+
+GameScreen.prototype.resize = function()
+{
+  if( this.subScreen.resize != undefined )
+  {
+    this.subScreen.resize();
+  }
+};
+
