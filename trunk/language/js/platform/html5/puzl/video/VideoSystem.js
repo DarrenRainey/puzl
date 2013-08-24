6ec337d11237775944dfb97a5f659951de5a4907
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2013-08-24 21:44:00 +0000

    Optimizations to adding dirty rectangles (rectangles inside others are no longer added--cuts out a lot of duplicates).

--- trunk/language/js/platform/html5/puzl/video/VideoSystem.js
+++ trunk/language/js/platform/html5/puzl/video/VideoSystem.js
@@ -151,58 +151,6 @@ function SetCanvasDimensions( canvas, width, height )
   canvas.height = height;
 }
 
-function DrawWithNearestScale( image, context, xPosition, yPosition, width, height )
-{
-  // Create an offscreen canvas, draw an image to it, and fetch the pixels
-  var offtx = document.createElement( "canvas" ).getContext( "2d" );
-  offtx.drawImage( image, 0, 0 );
-
-  var imgData;
-  try
-  {
-    imgData = offtx.getImageData( 0, 0, image.width, image.height ).data;
-  }
-  catch( error )
-  {
-    // Just draw image the regular way.
-    context.drawImage( image, xPosition, yPosition, width, height );
-    return;
-  }
-
-  var zoomXLevel = width  / image.width;
-  var zoomYLevel = height / image.height;
-
-//   var zoomX
-//
-//   var i;
-//   var r;
-//   var g;
-//   var b;
-//   var a;
-
-
-  // Draw the zoomed-up pixels to a different canvas context
-  for( var x = 0; x < image.width; ++x )
-  {
-    for( var y = 0; y < image.height; ++y )
-    {
-      // Find the starting index in the one-dimensional image data
-      var i = ( y * image.width + x ) * 4;
-      var r = imgData[i    ];
-      var g = imgData[i + 1];
-      var b = imgData[i + 2];
-      var a = imgData[i + 3];
-
-      context.fillStyle = "rgba(" + r + "," + g + "," + b + "," + ( a / 255 ) + ")";
-      context.fillRect( xPosition + ( x * zoomXLevel ), // TODO: Optimize.
-                        yPosition + ( y * zoomYLevel ),
-                        zoomXLevel, zoomYLevel );
-    }
-  }
-
-  //document.destroyElement( "OffscreenCanvas" );
-}
-
 //var offscreenCanvas        = document.createElement( "canvas" );
 //var offscreenCanvasContext = offscreenCanvas.getContext( "2d" );
 
@@ -225,69 +173,6 @@ function DrawWithNearestScale( sourceDrawObject, targetDrawObject, sourceXPositi
   context.mozImageSmoothingEnabled    = false;
   context.webkitImageSmoothingEnabled = false;
   context.drawImage( image, sourceXPosition, sourceYPosition, sourceWidth, sourceHeight, xPosition, yPosition, width, height );
-  return;
-
-  /*if( ( sourceHeight == height ) && ( sourceWidth == width ) )
-  {
-    // Just draw image the regular way.
-    context.drawImage( image, sourceXPosition, sourceYPosition, sourceWidth, sourceHeight, xPosition, yPosition, width, height );
-  }
-
-  // Create an offscreen canvas, draw an image to it, and fetch the pixels
-  offscreenCanvasContext.width  = sourceWidth;
-  offscreenCanvasContext.height = sourceHeight;
-  offscreenCanvasContext.clearRect( 0, 0, sourceWidth, sourceHeight );
-
-  offscreenCanvasContext.drawImage( image, sourceXPosition, sourceYPosition, sourceWidth, sourceHeight, 0, 0, sourceWidth, sourceHeight );
-
-  var imgData;
-  try
-  {
-    imgData = offscreenCanvasContext.getImageData( 0, 0, sourceWidth, sourceHeight ).data;
-  }
-  catch( error )
-  {
-    // Just draw image the regular way.
-    context.drawImage( image, sourceXPosition, sourceYPosition, sourceWidth, sourceHeight, xPosition, yPosition, width, height );
-    return;
-  }
-
-  var zoomXLevel = width  / sourceWidth;
-  var zoomYLevel = height / sourceHeight;
-
-  var zoomXIncrement = 0;
-  var zoomYIncrement = 0;
-
-  var i;
-  var r;
-  var g;
-  var b;
-  var a;
-
-  // Draw the zoomed-up pixels to a different canvas context
-  for( var x = 0; x < sourceWidth; ++x )
-  {
-    zoomXIncrement = x * zoomXLevel;
-
-    for( var y = 0; y < sourceHeight; ++y )
-    {
-      zoomYIncrement = y * zoomYLevel;
-
-      // Find the starting index in the one-dimensional image data
-      i = ( y * sourceWidth + x ) << 2;
-      r = imgData[i    ];
-      g = imgData[i + 1];
-      b = imgData[i + 2];
-      a = imgData[i + 3];
-
-      context.fillStyle = "rgba(" + r + "," + g + "," + b + "," + ( a / 255 ) + ")";
-      context.fillRect( xPosition + ( zoomXIncrement ), // TODO: Optimize.
-                        yPosition + ( zoomYIncrement ),
-                        zoomXLevel, zoomYLevel );
-    }
-  }
-
-  //document.destroyElement( "OffscreenCanvas" );*/
 }
 
 function BuildRgb( red, green, blue )
