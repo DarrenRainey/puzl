99e2cddc1982abc9394de89665fcee297ca10834
puzzud <puzzud@59a04a62-b934-65fe-292b-643986e4ebf1>
2013-08-31 14:52:39 +0000

    Setting color for color keyed video cell image layers no longer uses getImageData and putImageData to extract color key from source image and set appropriate color each call to setColor. Instead, the extraction is done once and a fillRect of the appropriate color with globalCompositing of "source-atop" is used.

--- language/js/platform/html5/puzl/video/VideoCellImage.js
+++ language/js/platform/html5/puzl/video/VideoCellImage.js
@@ -18,7 +18,7 @@ function VideoCellImage( sourceVideoObject, videoCellImageData )
   this.mapWidth;
   this.mapHeight;
 
-  this.canvas;
+  this.layerCanvas;
 
   this.attributes;
 
@@ -76,12 +76,14 @@ function VideoCellImage( sourceVideoObject, videoCellImageData )
 
   // Populate layer information.
   this.color = new Array();
-  this.canvas = new Array();
+  this.layerCanvas = new Array();
   this.colorKey = new Array();
   
   var layers = videoCellImageData["layers"];
   if( layers !== undefined )
   {
+    var sourceVideoObjectCanvas = this.sourceVideoObject.getCanvas();
+    
     var numberOfLayers = layers.length;
     var layerIndex;
     for( layerIndex = 0; layerIndex < numberOfLayers; layerIndex++ )
@@ -90,14 +92,42 @@ function VideoCellImage( sourceVideoObject, videoCellImageData )
       //console.log( layer["name"] );
       var canvas = CreateOffScreenCanvas();
       SetCanvasDimensions( canvas, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
-      this.canvas.push( canvas );
+      this.layerCanvas.push( canvas );
+
+      var canvasContext = GetCanvasContext2D( this.layerCanvas[layerIndex] );
+      canvasContext.drawImage( sourceVideoObjectCanvas, 0, 0 );
 
       var colorKey = layer["colorKey"];
       if( colorKey !== undefined )
       {
         colorKey = new Color( colorKey );
         this.colorKey.push( colorKey );
-        this.setColor( colorKey, layerIndex );
+
+        var imageData = canvasContext.getImageData( 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
+        var data = imageData.data;
+
+        var redKey   = colorKey.red;
+        var greenKey = colorKey.green;
+        var blueKey  = colorKey.blue;
+
+        var dataLength = data.length;
+        for( var i = 0; i < dataLength; i += 4 )
+        {
+          if( ( data[i] !== redKey ) ||
+              ( data[i + 1] !== greenKey ) ||
+              ( data[i + 2] !== blueKey ) )
+          {
+            data[i] = 0;
+            data[i + 1] = 0;
+            data[i + 2] = 0;
+            data[i + 3] = 0;
+          }
+        }
+
+        // overwrite original image
+        canvasContext.putImageData( imageData, 0, 0 );
+        
+        this.color.push( colorKey );
         //this.attributes &= ~IMAGE_ATTRIBUTE_COLOR;
       }
       else
@@ -113,7 +143,7 @@ function VideoCellImage( sourceVideoObject, videoCellImageData )
 
     var canvas = CreateOffScreenCanvas();
     SetCanvasDimensions( canvas, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
-    this.canvas.push( canvas );
+    this.layerCanvas.push( canvas );
     this.colorKey.push( undefined );
     this.color.push( new Color( 255, 255, 255, 1 ) );
   }
@@ -151,109 +181,63 @@ VideoCellImage.prototype.setColor = function( color, layerIndex )
   }
   
   var thisColor = this.color[layerIndex];
-  
-  var shouldChangeColor;
-  if( thisColor === undefined )
+  if( thisColor.equals( color ) )
   {
-    this.color[layerIndex] = thisColor = new Color( color );
-    shouldChangeColor = true;
+    return;
   }
-  else
-  if( !thisColor.equals( color ) )
-  {
-    thisColor.copy( color );
-    shouldChangeColor = true;
-  }
-  else
-  {
-    shouldChangeColor = false;
-  }
-  
-  if( shouldChangeColor )
-  {
-    //console.log( "Changing layer #" + layerIndex + " to " + thisColor.string );
 
-    var sourceVideoObjectCanvas = this.sourceVideoObject.getCanvas();
-    var canvasContext = GetCanvasContext2D( this.canvas[layerIndex] );
+  thisColor.copy( color );
+  
+  //console.log( "Changing layer #" + layerIndex + " to " + thisColor.string );
 
+  var canvas = this.layerCanvas[layerIndex];
+  var canvasContext = GetCanvasContext2D( canvas );
+
+  var colorKey = this.colorKey[layerIndex];
+  if( colorKey !== undefined )
+  {
+    canvasContext.globalCompositeOperation = "source-atop"; // NOTE: CocoonJS doesn't seem to do "source-in".
+    canvasContext.fillStyle = thisColor.string;
+    canvasContext.fillRect( 0, 0, canvas.width, canvas.height );
+  }
+  //else
+  //if( ( this.attributes & IMAGE_ATTRIBUTE_COLOR ) > 0 )
+  /*if( true )
+  {
     canvasContext.drawImage( sourceVideoObjectCanvas, 0, 0 );
+    
+    var imageData = canvasContext.getImageData( 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
+    var data = imageData.data;
 
-    var colorKey = this.colorKey[layerIndex];
-    if( colorKey !== undefined )
+    var red   = thisColor.red;
+    var green = thisColor.green;
+    var blue  = thisColor.blue;
+
+    var tintLevel = .125;
+    var tintLevelComplement = 1 - tintLevel;
+    for( var i = 0; i < data.length; i += 4 )
     {
-      var imageData = canvasContext.getImageData( 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
-      var data = imageData.data;
-
-      var red   = thisColor.red;
-      var green = thisColor.green;
-      var blue  = thisColor.blue;
-      var alpha = ( thisColor.alpha * 255 ) | 0;
-
-      var redKey   = colorKey.red;
-      var greenKey = colorKey.green;
-      var blueKey  = colorKey.blue;
-      
-      for( var i = 0; i < data.length; i += 4 )
-      {
-        if( ( data[i] === redKey ) &&
-            ( data[i + 1] === greenKey ) &&
-            ( data[i + 2] === blueKey ) )
-        {
-          data[i] = red;
-          data[i + 1] = green;
-          data[i + 2] = blue;
-          data[i + 3] = 255;//alpha;
-        }
-        else
-        {
-          data[i] = 0;
-          data[i + 1] = 0;
-          data[i + 2] = 0;
-          data[i + 3] = 0;
-        }
-      }
-
-      // overwrite original image
-      canvasContext.putImageData( imageData, 0, 0 );
-    }
-    //else
-    //if( ( this.attributes & IMAGE_ATTRIBUTE_COLOR ) > 0 )
-    /*if( true )
-    {
-      var imageData = canvasContext.getImageData( 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
-      var data = imageData.data;
-
-      var red   = thisColor.red;
-      var green = thisColor.green;
-      var blue  = thisColor.blue;
-      
-      var tintLevel = .125;
-      var tintLevelComplement = 1 - tintLevel;
-      for( var i = 0; i < data.length; i += 4 )
-      {
-        // red
-        data[i]     *= tintLevelComplement + ( red   * tintLevel );
-        // green
-        data[i + 1] *= tintLevelComplement + ( green * tintLevel );
-        // blue
-        data[i + 2] *= tintLevelComplement + ( blue  * tintLevel );
-      }
-
-      // overwrite original image
-      canvasContext.putImageData( imageData, 0, 0 );
-    }*/
-    else
-    {
-      //canvasContext.globalCompositeOperation = "source-atop";
-      //canvasContext.fillStyle = thisColor.string;
-      canvasContext.drawImage( sourceVideoObjectCanvas, 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
+      // red
+      data[i]     *= tintLevelComplement + ( red   * tintLevel );
+      // green
+      data[i + 1] *= tintLevelComplement + ( green * tintLevel );
+      // blue
+      data[i + 2] *= tintLevelComplement + ( blue  * tintLevel );
     }
 
-    var thisTargetVideoObject = this.targetVideoObject;
-    if( thisTargetVideoObject )
-    {
-      thisTargetVideoObject.addDirtyRectangle( this );
-    }
+    // overwrite original image
+    canvasContext.putImageData( imageData, 0, 0 );
+  }*/
+  else
+  {
+    var sourceVideoObjectCanvas = this.sourceVideoObject.getCanvas();
+    canvasContext.drawImage( sourceVideoObjectCanvas, 0, 0, sourceVideoObjectCanvas.width, sourceVideoObjectCanvas.height );
+  }
+
+  var thisTargetVideoObject = this.targetVideoObject;
+  if( thisTargetVideoObject )
+  {
+    thisTargetVideoObject.addDirtyRectangle( this );
   }
 };
 
@@ -282,5 +266,5 @@ VideoCellImage.prototype.getNumberOfCells = function()
 
 VideoCellImage.prototype.getCanvas = function()
 {
-  return this.canvas[0];
+  return this.layerCanvas[0];
 };
